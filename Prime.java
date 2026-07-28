6dfdc9bc415f8c51541ed3bbb558b304f5d3d935
hemarathinavel <hema05r@gmail.com>
2026-07-28 11:39:41 +0530

    add file  Prime

--- Prime.java
+++ Prime.java
@@ -6,10 +6,24 @@ public class Prime {
         System.out.println("Enter a number:");
         int n=scan.nextInt();
         int count=0;
-     if (n<=1) {
-
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+                count++;
+        }
+        if (count==2) 
+        {
+            System.out.println(n+ "is prime number");
+            
+        } 
+        else 
+        {
+            System.out.println(n+ "is not a prime number");
+            
+        }
+        scan.close();
         
     }
     }
     
-}
+
