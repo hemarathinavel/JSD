a017f5460019aa6bc3a0763958f075be4cf3e8a5
hemarathinavel <hema05r@gmail.com>
2026-07-28 11:04:39 +0530

    add file Fibboseries

---
+++ Prime.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=scan.nextInt();
+        int count=0;
+     if (n<=1) {
+
+        
+    }
+    }
+    
+}
