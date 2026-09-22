435eef006b0740ab3ca2c4c8490a88d13782bbb0
hemarathinavel <hema05r@gmail.com>
2026-09-22 15:33:58 +0530

    add file pgrm16.js

---
+++ Script.js/pgrm15.js
@@ -0,0 +1,26 @@
+//parameters and arguments
+/*function add(a,b)
+{
+    return  a+b;
+}
+let result= add(10,5);
+console.log(result);*/
+/*function multiply(a,b)
+{
+    return a*b;
+}
+let result=multiply(4,5);
+console.log(result);
+*/
+/*function square(num)
+{
+    return num*num;
+}
+let result=square(5);
+console.log(result);*/
+function sub(a,b)
+{
+    return a-b;
+}
+let result=sub(20,8)
+console.log(result);
---
+++ Script.js/pgrm16.js
@@ -0,0 +1,5 @@
+function greet(name="Student")
+{
+    console.log("Hello"+name);
+}
+greet();
